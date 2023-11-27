12d4ad7d1f0ccbb67feb019bcfa7b3c9c6ada8f8
Akramhn <akram.hadjnaser@esprit.tn>
2023-11-27 19:32:18 +0100

    List-bloc

---
+++ src/app/Back/bloc/list-bloc-back/list-bloc-back.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-bloc-back',
+  templateUrl: './list-bloc-back.component.html',
+  styleUrls: ['./list-bloc-back.component.css']
+})
+export class ListBlocBackComponent {
+
+}
