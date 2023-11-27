1854ef2462d9cb30be98fee88fca152c0c91b65b
Malak-hadj-messaoud <hadjmessaoud.malak@esprit.tn>
2023-11-27 19:17:17 +0100

    University Crud Back Init

---
+++ src/app/Back/university/list-university/list-university.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-university',
+  templateUrl: './list-university.component.html',
+  styleUrls: ['./list-university.component.css']
+})
+export class ListUniversityComponent {
+
+}
