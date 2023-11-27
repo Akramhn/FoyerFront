df4e4d623e3d3b995cf07b24588457f3a25739a5
unknown <amrihaithemm@gmail.com>
2023-11-27 22:30:53 +0100

    commit 1

---
+++ src/app/Back/evenement/add-evenement/add-evenement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-evenement',
+  templateUrl: './add-evenement.component.html',
+  styleUrls: ['./add-evenement.component.css']
+})
+export class AddEvenementComponent {
+
+}
---
+++ src/app/Back/evenement/list-evenement/list-evenement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-evenement',
+  templateUrl: './list-evenement.component.html',
+  styleUrls: ['./list-evenement.component.css']
+})
+export class ListEvenementComponent {
+
+}
