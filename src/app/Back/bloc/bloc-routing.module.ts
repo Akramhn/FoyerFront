143c3a39941d7adf82b6b516adbc306421286089
Akramhn <akram.hadjnaser@esprit.tn>
2023-11-28 12:01:13 +0100

    add bloc 2.0

--- src/app/Back/bloc/bloc-routing.module.ts
+++ src/app/Back/bloc/bloc-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BackoffComponent } from '../backoff/backoff.component';
 import { ListBlocBackComponent } from './list-bloc-back/list-bloc-back.component';
+import { AddBlocBackComponent } from './add-bloc-back/add-bloc-back.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BackoffComponent,
     children: [{ path: '', component: ListBlocBackComponent }],
+  },
+  {
+    path: '',
+    component: BackoffComponent,
+    children: [{ path: 'addBloc', component: AddBlocBackComponent }],
   },
 ];
 
