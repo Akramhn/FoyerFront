ae2d160bc7f5a1eff05b8b5c7b53e75a5d0cf5cf
Oussemamri <oussema9100@gmail.com>
2023-11-28 20:54:59 +0100

    second merge event

--- src/app/Back/evenement/evenement.module.ts
+++ src/app/Back/evenement/evenement.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { EvenementRoutingModule } from './evenement-routing.module';
 import { ListEvenementComponent } from './list-evenement/list-evenement.component';
+import { UpdateEvenementComponent } from './update-evenement/update-evenement.component';
 
 
 @NgModule({
-  declarations: [ListEvenementComponent],
+  declarations: [ListEvenementComponent, UpdateEvenementComponent],
   imports: [
     CommonModule,
     EvenementRoutingModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { UpdateEvenementComponent } from './Back/evnement/update-evenement/update-evenement.component';
 
 
 
@@ -67,6 +68,7 @@ register();
     DashboardComponent,
     ContactComponent,
     GetStartedComponent,
+    UpdateEvenementComponent,
   ],
   imports: [
     BrowserModule,
