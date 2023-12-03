a39095c685e20ccd6f68579db9c6c4ec199c3df9
Malak-hadj-messaoud <hadjmessaoud.malak@esprit.tn>
2023-12-03 22:09:37 +0100

    add universite  with image done

--- src/app/Back/university/university-routing.module.ts
+++ src/app/Back/university/university-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BackoffComponent } from '../backoff/backoff.component';
 import { ListUniversityComponent } from './list-university/list-university.component';
+import { AddUniversityComponent } from './add-university/add-university.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BackoffComponent,
     children: [{ path: '', component: ListUniversityComponent }],
+  },
+  {
+    path: '',
+    component: BackoffComponent,
+    children: [{ path: 'addUniversite', component: AddUniversityComponent }],
   },
 ];
 
